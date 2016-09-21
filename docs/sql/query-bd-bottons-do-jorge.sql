38fa3ed454d0089bcb475623f50c7b445d11c711
Pedro Oliveira <pedro.gdstr@gmail.com>
2016-09-21 11:43:35 -0300

    Query sql

--- docs/sql/query-bd-bottons-do-jorge.sql
+++ docs/sql/query-bd-bottons-do-jorge.sql
@@ -1,92 +1,60 @@
-CREATE TABLE ProdutoCarrinho(
-produtoId INT NOT NULL,
-carrinhoId INT NOT NULL,
-quantidade INT NOT NULL,
-PRIMARY KEY(produtoId, carrinhoId)
-)
-
 CREATE TABLE Usuario(
-id INT IDENTITY(1,1) NOT NULL,
-nome VARCHAR(100) NOT NULL,
-carrinho_id INT NOT NULL,
-produtoCarrinho_ProdutoId INT NOT NULL,
-produtoCarrinho_CarrinhoId INT NOT NULL,
-adm BIT NOT NULL,
-aspnet_Users_UserId uniqueidentifier NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY(produtoCarrinho_ProdutoId, produtoCarrinho_CarrinhoId) REFERENCES ProdutoCarrinho(produtoId, carrinhoId),
-FOREIGN KEY(aspnet_Users_UserId) REFERENCES aspnet_Users(UserID)
+	id INT IDENTITY PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	dataRegistro DATETIME NOT NULL,
+	adm BIT NOT NULL,
+	aspnet_id UNIQUEIDENTIFIER,
+	FOREIGN KEY (aspnet_id) REFERENCES aspnet_Users(UserId) 
 )
-
 CREATE TABLE Telefone(
-id INT IDENTITY(1,1) NOT NULL,
-numero VARCHAR(100) NOT NULL,
-usuario_id INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY(usuario_id) REFERENCES Usuario(id)
+	id INT IDENTITY PRIMARY KEY,
+	numero VARCHAR(11) NOT NULL,
+	Usuario_id INT NOT NULL, 
+	FOREIGN KEY (Usuario_id) REFERENCES Usuario(id)
 )
-
 CREATE TABLE Mensagem(
-id INT IDENTITY(1,1) NOT NULL,
-data DATE NOT NULL,
-corpo VARCHAR(1000) NOT NULL,
-usuarioDestinatario INT NOT NULL,
-usuarioRemetente INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY(usuarioDestinatario) REFERENCES Usuario(id),
-FOREIGN KEY(usuarioRemetente) REFERENCES Usuario(id)
+	id INT IDENTITY PRIMARY KEY,
+	data DATETIME NOT NULL,
+	corpo TEXT NOT NULL,
+	visualizada BIT NOT NULL,
+	UsuarioDestinatario_id INT NOT NULL,	
+	UsuarioRemetente_id INT NOT NULL,
+	FOREIGN KEY (UsuarioDestinatario_id) REFERENCES Usuario(id),
+	FOREIGN KEY (UsuarioRemetente_id) REFERENCES Usuario(id)
 )
-
-CREATE TABLE EncStatus(
-id INT IDENTITY(1,1) NOT NULL,
-descricao VARCHAR(45) NOT NULL,
-PRIMARY KEY(id)
+CREATE TABLE localEntrega(	
+	id INT IDENTITY PRIMARY KEY,
+	rua VARCHAR(50) NOT NULL,
+	numero INT NOT NULL,
+	bairro VARCHAR(30) NOT NULL,
+	complemento VARCHAR(50) NOT NULL,
+	descricao VARCHAR(50) NOT NULL,
+	cep VARCHAR(8) NOT NULL
 )
-
-CREATE TABLE LocalEntrega(
-id INT IDENTITY(1,1) NOT NULL,
-rua VARCHAR(45) NOT NULL,
-numero INT NOT NULL,
-bairro VARCHAR(45) NOT NULL,
-complemento VARCHAR(45),
-descricao VARCHAR(45),
-cep VARCHAR(8),
-PRIMARY KEY(id)
+CREATE TABLE Status(	
+	id INT IDENTITY PRIMARY KEY,
+	descricao VARCHAR(20) NOT NULL
 )
-
-CREATE TABLE Encomenda(
-id INT IDENTITY(1,1) NOT NULL,
-precoTotal NUMERIC(10,2) NOT NULL,
-dataEntrega DATE NOT NULL,
-statusId INT NOT NULL,
-localEntregaId INT NOT NULL,
-usuarioId INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY (statusId) REFERENCES EncStatus(id),
-FOREIGN KEY (localEntregaId) REFERENCES LocalEntrega(id),
-FOREIGN KEY (usuarioId) REFERENCES Usuario(id)
+CREATE TABLE Encomenda(	
+	id INT IDENTITY PRIMARY KEY,
+	precoTotal NUMERIC(10,2) NOT NULL,
+	dataEntrega DATETIME,
+	Usuario_id INT NOT NULL,
+	localEntrega_id INT NOT NULL,
+	Status_id INT NOT NULL,
+	FOREIGN KEY (Usuario_id) REFERENCES Usuario(id),
+	FOREIGN KEY (localEntrega_id) REFERENCES localEntrega(id),
+	FOREIGN KEY (Status_id) REFERENCES Status(id)
 )
-
-CREATE TABLE Produto(
-id INT IDENTITY(1,1) NOT NULL,
-descricao VARCHAR(200) NOT NULL,
-imagem VARCHAR(1000) NOT NULL,
-tamanho VARCHAR(45) NOT NULL,
-preco NUMERIC(10,2) NOT NULL,
-PRIMARY KEY(id)
-)
-
 CREATE TABLE Marcador(
-id INT IDENTITY(1,1) NOT NULL,
-marcador VARCHAR(45) NOT NULL,
-Produto_id INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY(Produto_id) REFERENCES Produto(id)
+	id INT IDENTITY PRIMARY KEY,
+	descricao VARCHAR(20) NOT NULL
 )
-
-CREATE TABLE ProdutoEncomenda(
-encomenda_id INT NOT NULL,
-produto_id INT NOT NULL,
-quantidade INT NOT NULL,
-PRIMARY KEY(encomenda_id, produto_id)
+CREATE TABLE Produto(
+	id INT IDENTITY PRIMARY KEY,
+	descricao VARCHAR(20) NOT NULL,
+	imagem VARCHAR(100) NOT NULL	
+)
+CREATE TABLE marcadorProduto(
+	produto_id INT PRIMARY KEY
 )
