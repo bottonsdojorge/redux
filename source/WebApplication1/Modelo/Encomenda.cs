64dea76830b2d60f24ebabdaa0a516c9f2b44bf5
BiGGLeo <leonzorx@gmail.com>
2016-10-07 10:09:24 -0300

    Implementação DAO
    Status e Usuário implementados

---
+++ source/WebApplication1/Modelo/Encomenda.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Modelo
+{
+    public class Encomenda
+    {
+        private Usuario _Usuario;
+        public Usuario Usuario 
+        {
+            get { return _Usuario; }
+            set { _Usuario = value; }
+        }
+
+        private double _precoTotal;
+        public double precoTotal 
+        {
+            get { return _precoTotal; }
+            set { _precoTotal = value; }
+        }
+
+        private DateTime _dataEntrega;
+        public DateTime dataEntrega 
+        {
+            get { return _dataEntrega; }
+            set { _dataEntrega = value; }
+        }
+
+        private localEntrega _localEntrega;
+        public localEntrega localEntrega 
+        {
+            get { return _localEntrega; }
+            set { _localEntrega = value; }
+        }
+
+        private Status _Status;
+        public Status Status {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+
+        public Encomenda()
+        {
+            this.Usuario = new Usuario();
+            this.precoTotal = 0;
+            this.dataEntrega = default(DateTime);
+            this.localEntrega = new localEntrega();
+            this.Status = new Status();
+        }
+
+
+    }
+}
--- source/WebApplication1/Modelo/Usuario.cs
+++ source/WebApplication1/Modelo/Usuario.cs
@@ -7,5 +7,47 @@ namespace WebApplication1.Modelo
 {
     public class Usuario
     {
+        private int _id;
+        public int id 
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _aspnet_id;
+        public string aspnet_id 
+        {
+            get { return _aspnet_id; }
+            set { _aspnet_id = value; }
+        }
+        private string _nome;
+        public string nome 
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        private List<Telefone> _telefones;
+
+        public List<Telefone> telefones
+        {
+            get { return _telefones; }
+            set { _telefones = value; }
+        }
+        
+        public Usuario()
+        {
+            this.id = 0;
+            this.aspnet_id = "";
+            this.nome = "";
+            this.telefones = new List<Telefone>();
+        }
+        public Usuario(int id, string aspnet_id, string nome, List<Telefone> telefones)
+        {
+            this.id = id;
+            this.aspnet_id = aspnet_id;
+            this.nome = nome;
+            this.telefones = telefones;
+        }
+
     }
 }
