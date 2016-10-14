db6409b1fe3cbd9d2c8734ef145cc64b61af531c
JorgeEnrique21 <jorgenriqueazevedotinoco@gmail.com>
2016-10-14 10:26:44 -0300

    dal carrinho

--- source/WebApplication1/DAL/DALCarrinho.cs
+++ source/WebApplication1/DAL/DALCarrinho.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,6 @@ namespace WebApplication1.DAL
 {
     public class DALCarrinho
     {
+
     }
 }
--- source/WebApplication1/Modelo/Carrinho.cs
+++ source/WebApplication1/Modelo/Carrinho.cs
@@ -14,11 +14,32 @@ namespace WebApplication1.Modelo
             set { _Usuario_id = value; }
         }
 
+        private List<KeyValuePair<Item, int>> _itens;
+        public List<KeyValuePair<Item, int>> itens
+        {
+            get { return _itens; }
+            set { _itens = value; }
+        }
+
         private double _precoTotal;
         public double precoTotal
         {
             get { return _precoTotal; }
             set { _precoTotal = value; }
+        }
+
+        public Carrinho ()
+	    {
+            this.itens = new List<KeyValuePair<Item,int>>();
+            this.precoTotal = 0;
+            this.Usuario_id = 0;
+	    }
+
+        public Carrinho(List<KeyValuePair<Item, int>> itens, int precoTotal, int Usuario_id)
+        {
+            this.itens = itens;
+            this.precoTotal = precoTotal;
+            this.Usuario_id = Usuario_id;
         }
     }
 }
