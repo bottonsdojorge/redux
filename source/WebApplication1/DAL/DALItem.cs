1393e401734f7532366004932389afed080df161
nestorcaetano <14nestorcaetano@gmail.com>
2016-10-18 15:24:20 -0300

    DALItem

--- source/WebApplication1/DAL/DALItem.cs
+++ source/WebApplication1/DAL/DALItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
@@ -63,6 +64,53 @@ namespace WebApplication1.DAL
                 throw;
             }
             return itens;
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public Modelo.Item Select(int idItem)
+        {
+            // O Produto retorno
+            Modelo.Item item = new Modelo.Item(); ;
+
+            // A conexão
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                using (conn)
+                {
+                    // O SQL
+                    string sqlItens = "SELECT * FROM Item WHERE id = @id";
+                    SqlCommand cmdItens = new SqlCommand(sqlItens);
+                    cmdItens.Parameters.Add("@id", SqlDbType.Int).Value = idItem;
+                    SqlDataReader drItens;
+                    using (drItens = cmdItens.ExecuteReader())
+                    {
+                        // Leitura do resultado
+                        if (drItens.HasRows)
+                        {
+                            while (drItens.Read())
+                            {
+                                int idProduto = Convert.ToInt32(drItens["Produto_id"]);
+                                int idTamanho = Convert.ToInt32(drItens["Tamanho_id"]);
+
+                                DALProduto dalProduto = new DALProduto();
+                                DALTamanho dalTamanho = new DALTamanho();
+
+                                Modelo.Produto produto = dalProduto.Select(idProduto);
+                                Modelo.Tamanho tamanho = dalTamanho.Select(idTamanho);
+                                
+                                item = new Modelo.Item(produto, tamanho);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SystemException)
+            {
+                throw;
+            }
+            return item;
         }
     }
 }
